35bd97415f356018670e9187286d101d9d4a8dad
sukanya2712 <sukanyanaik511@gmail.com>
2023-04-26 01:25:22 +0530

    added uc13

--- javascript/js1.js
+++ javascript/js1.js
@@ -1,6 +1,47 @@
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function() {
-output.textContent = salary.value;
-});
+const save = () => {
+   try{
+       let EmployeePayrollData=EmployeePayrollData();
+      
+   }catch (e) {
+       return;
+       }
+   }
+   
+   const createEmployeePayroll= () => {
+       let employeePayrollData= new EmployeePayrollData();
+       try{
+           employeePayrollData.name = getInputValueById('#name');
+       } catch (e) {
+           setTextValue('.text-error',e);
+           throw e;
+       }
+       employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+       employeePayrollData.gender=getSelectedValues('[name=gender]').pop();
+       employeePayrollData.department=getSelectedValues('[name=department]');
+       employeePayrollData.salary=getInputValueById('#salary');
+       employeePayrollData.note=getInputValueById('#notes');
+       let date = getInputValueById('#day')+" "+getInputValueById('#month')+" "+
+                   getInputValueById('#year');
+       employeePayrollData.date=Date.parse(date);  
+       alert(employeePayrollData.toString());
+       return employeePayrollData;
+   }
+   
+   const getSelectedValues = (propertyValue) => {
+       let allItems = document.querySelectorAll(propertyValue);
+       let selItems = [];
+       allItems.forEach(item => { 
+           if(item.checked) selItems.push(item.value);
+       });
+       return selItems;
+   }
+   
+   const getInputValueById= (id) => {
+       let value = document.querySelector(id).value;
+       return value;
+   }
+   
+   const getInputElementValue = (id) => {
+       let value = document.getElementById(id).value;
+       return value;
+   }
